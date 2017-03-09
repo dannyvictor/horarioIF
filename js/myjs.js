5e097782ccd7d4adadd4de89e4d21c68824efa3a
Danny Fernandes <daniellevictorf@gmail.com>
2017-03-09 10:42:26 -0300

    Update myjs.js

--- js/myjs.js
+++ js/myjs.js
@@ -1,13 +1,9 @@
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-
-function filterFunction() {
+function filterFunction(n,i) {
     var input, filter, ul, li, p, i;
-    input = document.getElementById("myInput");
+    input = document.getElementById(i);
     filter = input.value.toUpperCase();
-    div = document.getElementById("myDropdown");
-    p = div.getElementsByTagName("a");
+    div = document.getElementById(n);
+    p = div.getElementsByTagName("p");
     for (i = 0; i < p.length; i++) {
         if (p[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
             p[i].style.display = "";
